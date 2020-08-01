8aad213776292f45426011c80a2757cf6308ec13
akshaya <AkshayA@talentica-all.com>
2020-08-02 00:21:04 +0530

    initialized

---
+++ mission-frontend/src/store/selectors/authSelectors.js
@@ -0,0 +1,7 @@
+import { createSelector } from 'reselect';
+
+const authSelector = state => state.auth;
+
+const authenticatedSelector = createSelector(authSelector, auth => auth.authToken !== null);
+
+export { authSelector, authenticatedSelector };
